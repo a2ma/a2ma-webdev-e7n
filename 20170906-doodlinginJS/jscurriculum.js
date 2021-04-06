783c02d90e7127fe5497d504cfc8fd697f6633c6
a2ma <a2ma@outlook.com>
2021-04-05 20:29:33 -0400

    add javascript exercises and portfolio edits

--- 20170906-doodlinginJS/jscurriculum.js
+++ 20170906-doodlinginJS/jscurriculum.js
@@ -2726,6 +2726,51 @@ function diffArray(arr1, arr2) {
 // console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 // console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 
+/* #############################################################################
+// // 2020-07-03: Intermediate Algorithm Scripting: Seek and Destroy
+#############################################################################*/
+function destroyer(arr) {
+  return arr.filter(item => {
+    let pass = true;
+    let i = 1;
+    while (pass && i < arguments.length) {
+      console.log("Item: " + item);
+      console.log(arguments[i]);
+      pass = !(item == arguments[i]);
+      i++;
+    }
+    return pass;
+  });
+}
+// console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+// console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+
+/* #############################################################################
+// // 2020-06-27: Intermediate Algorithm Scripting: Wherefore art thou
+#############################################################################*/
+ /* 
+Make a function that looks through an array of objects(first argument) and returns an array of all objects that have matching name and value pairs(second argument).Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+For example, if the first argument is[{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array(the first argument), because it contains the name and its value, that was passed on as the second argument.
+*/
+
+function whatIsInAName(collection, source) {
+  var arr = [];
+  // Only change code below this line
+  arr = collection.filter(item => {
+    for (let key in source) {
+      if (!item.hasOwnProperty(key) || item[key] != source[key]) {
+        return false;
+      }
+    }
+    return true;
+  });
+  // Only change code above this line
+  return arr;
+}
+
+// console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+// whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }) should return [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }].
+
 // ##############################################################################################################
 
 // Async Foundations
